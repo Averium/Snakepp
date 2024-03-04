e45101c27215957184076f1895d747381ef9bc5f
aron.solyomvari <solyomvaari@gmail.com>
2024-03-04 23:07:33 +0100

    updated comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,6 @@ FIXME:
 TODO:
     - UPGRADE MAKEFILE FOR AUTOMATIC DEPENDENCIES
 
-    - add unconfigured keypress mode to KeyboardHandler that tracks all keys, but only when the mode is active
-        - (text input and key config scenarios)
-
-    - keybind widget (datalabel subclass) or just use datalabel class and solve with that
-    - keybind window
-
     - text input widget (datalabel subclass)
 
     - highscores system
